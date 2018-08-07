4dd06fff3d5310b07935bbcbd63de3a876fbaca2
pavelkang <kaik1@andrew.cmu.edu>
2018-08-07 11:38:55 -0700

    init

---
+++ config/webpack.base.config.js
@@ -0,0 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader",
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: './test.html'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+}
